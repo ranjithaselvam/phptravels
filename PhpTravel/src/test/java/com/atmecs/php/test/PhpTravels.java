b21bc0ba006416ba7c76492742ed35c2ff57204b
ranjitha.selvam <ranjitha.selvam@ATMECSINLT-028.ATMECS.CORP>
2019-12-04 20:57:50 +0530

    updated

--- PhpTravel/src/test/java/com/atmecs/php/test/PhpTravels.java
+++ PhpTravel/src/test/java/com/atmecs/php/test/PhpTravels.java
@@ -1,5 +1,6 @@
 package com.atmecs.php.test;
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -17,11 +18,15 @@ import com.atmecs.php.helper.Waits;
 import com.atmecs.php.reports.Reports;
 
 public class PhpTravels extends Base {
+	List<String> leastPrice;
+	String split;
+	int index = 0;
 	public Reports report = new Reports();
 	public Helper helper = new Helper();
 	public Waits wait = new Waits();
 	public DateAndTime dateAndTime = new DateAndTime();
 	public FindObject find = new FindObject();
+	String detail="(//button[contains(text(),'Details')])[*]";
 
 	@BeforeClass
 	public void browserLaunch() {
@@ -37,7 +42,6 @@ public class PhpTravels extends Base {
 		report.logInfo("Step 3:Click on hotel");
 		helper.clickOnWebElement(driver, Constant.homePageLoc_file, "loc_hotel");
 		Actions action = new Actions(driver);
-		// action.sendKeys(Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB);
 
 		report.logInfo("Step 4:Verify user is landed into Home page ");
 		String currentUrl = driver.getCurrentUrl();
@@ -54,23 +58,18 @@ public class PhpTravels extends Base {
 		WebElement findElement = driver.findElement(By.xpath("(//input[@type='text'])[1]"));
 		findElement.sendKeys("bangalore");
 		Thread.sleep(2000);
-		// findElement.sendKeys(Keys.ENTER);
-		// helper.inputValuesToTheWebelement(driver, Constant.homePageLoc_file,
-		// "loc_destination","bangalore");
 		action.sendKeys(Keys.ENTER).build().perform();
 		Thread.sleep(5000);
 
 		report.logInfo("Step 6:Check-In Date should be greater than 10days");
 		helper.clickOnWebElement(driver, Constant.homePageLoc_file, "loc_checkin");
-		// String checkin = utils.propertyRead(Constant.homePageLoc_file,
-		// "checkindays");
+
 		String moreThanTenDays = dateAndTime.gettingDay("11");
 		helper.inputValuesToTheWebelement(driver, Constant.homePageLoc_file, "loc_checkin", moreThanTenDays);
 
 		report.logInfo("Step 7:Check-out Date should be +2 days of Check-In Date");
 		helper.clickOnWebElement(driver, Constant.homePageLoc_file, "loc_checkout");
-		// String checkout = utils.propertyRead(Constant.homePageLoc_file,
-		// "checkoutdays");
+
 		String plusTwoDays = dateAndTime.gettingDay("13");
 		helper.inputValuesToTheWebelement(driver, Constant.homePageLoc_file, "loc_checkout", plusTwoDays);
 
@@ -85,6 +84,18 @@ public class PhpTravels extends Base {
 		report.logInfo("Step 10:Perform Search for the Hotels");
 		helper.clickOnWebElement(driver, Constant.homePageLoc_file, "loc_search");
 
+		driver.findElement(By.cssSelector(".btn.btn-secondary.btn-wide.btn-toggle.btn-sm.btn-change-search")).click();
+		String destination = driver.findElement(By.cssSelector(".select2-input")).getAttribute("value");
+		System.out.println(destination);
+		String checkin = driver.findElement(By.cssSelector("div #checkin")).getAttribute("value");
+		System.out.println(checkin);
+		String checkout = driver.findElement(By.cssSelector("div #checkout")).getAttribute("value");
+		System.out.println(checkout);
+		String adult = driver.findElement(By.xpath("//input[@name='adults']")).getAttribute("value");
+		System.out.println(adult);
+		String children = driver.findElement(By.xpath("//input[@name='children']")).getAttribute("value");
+		System.out.println(children);
+
 	}
 
 	/**
@@ -96,24 +107,62 @@ public class PhpTravels extends Base {
 
 	@Test(priority = 2)
 	public void identifyHotel() throws InterruptedException {
-		for (int i = 0; i < 4; i++) {
-			Thread.sleep(2000);
-			WebElement findElement = driver.findElement(By.cssSelector(utils.propertyRead(Constant.hotelPageLoc_file, "loc_viewmore")));
+
+		driver.findElement(By.cssSelector(".cc-btn.cc-dismiss")).click();
+		
+		for (int viewmore = 0; viewmore < 4; viewmore++) {
+			WebElement findElement = driver.findElement(By.cssSelector(".btn.btn-block.btn-primary.btn-lg"));
+
 			JavascriptExecutor js = (JavascriptExecutor) driver;
-			js.executeScript("arguments[0].scrollIntoView(false);", findElement);
+			js.executeScript("window.scrollBy(0,1000)");
 			findElement.click();
-			report.logInfo("view more "+i+" loaded");
+			System.out.println("click" + viewmore + "view more");
 		}
+
 		report.logInfo("Step 14 :Identifing hotel having 4-star rating with low cost");
-		List<WebElement> ratingFromWebsite = driver
-				.findElements(By.xpath(utils.propertyRead(Constant.hotelPageLoc_file, "loc_rating")));
+		List<WebElement> ratingFromWebsite = driver.findElements(By.cssSelector("[data-stars='4']"));
 		for (WebElement fourStarRating : ratingFromWebsite) {
 
 			String allRatingValues = fourStarRating.getText();
 			System.out.println(allRatingValues);
 
+			
+
 		}
 
+		List<WebElement> priceFromWebSite = driver.findElements(By.cssSelector("div[data-stars='4'] .text-secondary"));
+		
+		List<Double> newPrice = new ArrayList<Double>();
+
+		for (WebElement price : priceFromWebSite) {
+			String text = price.getText();
+			String newValue = text.replace("USD ", "");
+			double var = Double.parseDouble(newValue);
+			newPrice.add(index, var);
+			index++;
+
+		}
+		Collections.sort(newPrice);
+		String lowPrice="USD "+newPrice.get(0);
+		
+		List<WebElement>priceList=driver.findElements(By.xpath("//span[contains(text(),'USD')]"));
+		for(int integer=1;integer<=priceList.size();integer++) {
+			String getPrice=driver.findElement(By.xpath("(//span[contains(text(),'USD')])["+integer+"]")).getText();
+		
+			if(getPrice.equalsIgnoreCase(lowPrice)) {
+				
+				String replace = detail.replace("*",integer+"");
+				WebElement element=driver.findElement(By.xpath(replace));
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true);", element);
+				js.executeScript("window.scrollBy(0,-75)");
+				element.click();
+				break;
+			}
+		}
+		
+		System.out.println("***THE END***");
 	}
 
 }
+
